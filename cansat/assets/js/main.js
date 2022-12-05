e643bb55a904627bb0b43e948078f3b4285166fe
Andrew Jolley <andrew.jolley145@gmail.com>
2022-12-05 20:47:52 +0000

    7.16

--- cansat/assets/js/main.js
+++ cansat/assets/js/main.js
@@ -21,4 +21,4 @@ function activateCountdown(element, dateString) {
     }, 250)
 }
 
-activateCountdown(document.getElementById("myCountdown"), "2022-03-22");
+activateCountdown(document.getElementById("myCountdown"), "2023-03-28");
